675641f12773229e242c6e0bde290bdd537bf830
Hjalti Magnusson <hjaltmann@gmail.com>
2014-04-19 22:45:33 +0000

    A little more testing

--- week12/code/exam2012/6/main.cpp
+++ week12/code/exam2012/6/main.cpp
@@ -8,5 +8,13 @@ using namespace std;
 int main()
 {
     HashTable<string> a;
-    a.find("asdf");
+    string s1 = "aaaa";
+    string s2 = "abaa";
+    string s3 = "aaba";
+    a.insert("asdf", s1);
+    a.insert("adf", s2);
+    a.insert("df", s3);
+    cout << *a.find("asdf") << endl;
+    cout << *a.find("adf") << endl;
+    cout << *a.find("df") << endl;
 }
