0799f75e527cbf1e692bb3852586b90ffd7ce2fe
Hjalti Magnusson <hjaltmann@gmail.com>
2015-01-26 12:41:17 +0000

    This is SPARTA

---
+++ week03/code/linkedlist3.cpp
@@ -0,0 +1,88 @@
+#include <iostream>
+using namespace std;
+
+struct Node
+{
+    int item;
+    Node *link;
+};
+
+typedef Node* NodePtr;
+
+class IntList
+{
+    private:
+        NodePtr head;
+
+    public:
+        void head_insert(int item) {
+            NodePtr tmp_ptr;
+            tmp_ptr = new Node;
+            tmp_ptr->item = item;
+            tmp_ptr->link = head;
+            head = tmp_ptr;
+        }
+
+        void print()
+        {
+            cout << "List: " << endl;
+            if(head == NULL) {
+                cout << "<empty>" << endl;
+            }
+
+            for(NodePtr iter = head; iter != NULL; iter = iter->link) {
+                cout << iter->item << endl;
+            }
+        }
+
+        NodePtr search(int target) {
+            NodePtr iter;
+            for(iter = head; iter != NULL; iter = iter->link) {
+                if(iter-> item == target) {
+                    return iter;
+                }
+            }
+            return NULL;
+        }
+
+        void insert(NodePtr after_me, int item) {
+            NodePtr tmp_ptr;
+            tmp_ptr = new Node;
+            tmp_ptr->item = item;
+
+            tmp_ptr->link = after_me->link;
+            after_me->link = tmp_ptr;
+        }
+
+        void head_remove() {
+            if(head == NULL) {
+                return;
+            }
+
+            NodePtr old_head = head;
+            head = head->link;
+
+            delete old_head;
+        }
+};
+
+int main() {
+    IntList lis;
+
+    for(int i = 0; i != 10; i++) {
+        lis.head_insert(i*i);
+    }
+
+    lis.print();
+    NodePtr result;
+    result = lis.search(81);
+    lis.insert(result, 35);
+    lis.print();
+    lis.insert(lis.search(0), 42);
+    lis.print();
+    for(int i = 0; i != 3; i++) {
+        lis.head_remove();
+    }
+    lis.print();
+}
+
---
+++ week03/code/linkedlist_rec.cpp
@@ -0,0 +1,98 @@
+#include <iostream>
+using namespace std;
+
+struct Node
+{
+    int data;
+    Node *link;
+};
+
+typedef Node* NodePtr;
+
+void head_insert(NodePtr& head, int data);
+void print(NodePtr head);
+NodePtr search(NodePtr head, int target);
+void insert(NodePtr after_me, int data);
+void head_remove(NodePtr& head);
+void remove_all(NodePtr head);
+
+int main() {
+    NodePtr head = NULL;
+
+    for(int i = 0; i != 10; i++) {
+        head_insert(head, i*i);
+    }
+
+    print(head);
+    NodePtr result;
+    result = search(head, 81);
+    insert(result, 35);
+    print(head);
+    insert(search(head,0), 42);
+    print(head);
+    for(int i = 0; i != 3; i++) {
+        head_remove(head);
+    }
+    print(head);
+    remove_all(head);
+}
+
+void head_insert(NodePtr& head, int data) {
+    NodePtr tmp_ptr;
+    tmp_ptr = new Node;
+    tmp_ptr->data = data;
+    tmp_ptr->link = head;
+    head = tmp_ptr;
+
+}
+
+void print(NodePtr head) {
+    if(head == NULL) {
+        cout << "NULL" << endl;
+    } else {
+        cout << head->data << "::";
+        print(head->link);
+    }
+
+}
+
+NodePtr search(NodePtr head, int target)
+{
+    if (head == NULL) {
+        return NULL;
+    }
+    if (head->data == target) {
+        return head;
+    }
+    return search(head->link, target);
+}
+
+void insert(NodePtr after_me, int data)
+{
+    NodePtr tmp_ptr;
+    tmp_ptr = new Node;
+    tmp_ptr->data = data;
+
+    tmp_ptr->link = after_me->link;
+    after_me->link = tmp_ptr;
+}
+
+void head_remove(NodePtr& head)
+{
+    if(head == NULL) {
+        return;
+    }
+
+    NodePtr old_head = head;
+    head = head->link;
+
+    delete old_head;
+}
+
+void remove_all(NodePtr head) {
+    if (head == NULL) {
+        return;
+    }
+    remove_all(head->link);
+    delete head;
+}
