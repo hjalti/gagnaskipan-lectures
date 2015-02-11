b61f929ee97b98785d6286a8fc21082220d4ffd4
Hjalti Magnusson <hjaltmann@gmail.com>
2015-02-11 23:37:30 +0000

    A few slides on valgrind errors

---
+++ valgrind/conditional_jump_or_move.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+
+using namespace std;
+
+int main() {
+    int a;
+    if (a == 3) {
+        cout << "A" << endl;
+    } else {
+        cout << "B" << endl;
+    }
+    return 0;
+}
